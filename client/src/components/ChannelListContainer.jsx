7ccb55085c79e1854dfabea707c77a124d1d73be
Nicholas igunbor <nickoklaus5@gmail.com>
2022-12-10 11:35:50 +0100

    added channel search jsx

--- client/src/components/ChannelListContainer.jsx
+++ client/src/components/ChannelListContainer.jsx
@@ -3,6 +3,7 @@ import { ChannelList, useChatContext } from 'stream-chat-react';
 import Cookies from 'universal-cookie';
 import { ChannelSearch, TeamChannelList, TeamChannelPreview } from './';
 import HospitalIcon from '../assets/hospital.png';
+import LogoutIcon from '../assets/logout.png';
 
 
 const Sidebar = () => (
@@ -12,15 +13,31 @@ const Sidebar = () => (
                 <img src={HospitalIcon} alt="Hospital" width="30"/>
             </div>
         </div>
+        <div className="channel-list__sidebar__icon2">
+            <div className='icon1__inner'>
+                <img src={LogoutIcon} alt="Logout" width="30"/>
+            </div>
+        </div>
     </div>
+)
+
+const CompanyHeader = () => (
+    <div className='channel-list__header'>
+        <p className='channel-list__header__text'>Medi Chat</p>
+    </div>
+
 )
 
 const ChannelListContainer = () => {
 
   return (
-    <div>
-        ChannelListContainer
+   <>
+    <Sidebar />
+    <div className='channel-list__list__wrapper'>
+        <CompanyHeader />
+        <ChannelSearch />
     </div>
+   </>
   )
 }
 
