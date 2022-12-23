29d0ea0fafe2f197258189ed8e88b3bc3cbdcf23
Nicholas igunbor <nickoklaus5@gmail.com>
2022-12-23 13:59:52 +0100

    finished the backend api

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -3,11 +3,23 @@ const bcrypt = require('bcrypt');
 const StreamChat = require('Stream-Chat');
 const crypto = require('crypto');
 
-const signup = () => {
-    try {
-        const {fullName, userName, password} = req.body;
+const api_key = process.env.STREAM_API_KEY;
+const api_secret = process.env.STREAM_API_SECRET;
+const app_id = process.env.STREAM_APP_ID;
 
-        const userId = crypto
+const signup = async(req, res) => {
+    try {
+        const {fullName, userName, password, phoneNumber} = req.body;
+
+        const userId = crypto.randomBytes(16).toString('hex'); // gives a unique user id
+
+        const serverClient = connect(api_key, api_secret, app_id);
+
+        const hashedPassword = await bcrypt.hash(password, 10);
+
+        const token = serverClient.createUserToken(userId);
+
+        res.status(200).json({token, fullName, userName, userId, hashedPassword, phoneNumber});
 
     } catch (error){
         console.log(error);
@@ -16,8 +28,26 @@ const signup = () => {
 
 };
 
-const login = () => { 
+const login = async(req, res) => { 
     try{
+        const { userName, password} = req.body;
+
+        const serverClient = connect(api_key, api_secret, app_id);
+        const client = StreamChat.getInstance(api_key, api_secret);
+
+        const { users } = await client.queryUsers({ name: userName });
+
+        if(!users.length) return res.status(400).json({message: "user not found"});
+
+        const success = await bcrypt.compare(password, users[0].hashedPassword);
+
+        const token = serverClient.createUserToken(users[0].id);
+
+        if(success){
+            res.status(200).json({ token, fullName: users[0].fullName, userName, userId: users[0].id});
+        } else {
+            res.status(500).json({message: "incorrect password"});
+        }
 
     } catch (error){
         console.log(error);
